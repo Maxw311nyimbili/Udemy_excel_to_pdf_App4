1a433b0020fb5cdd399f64c92112724f9588ad64
Maxwell <130581484+Maxw311nyimbili@users.noreply.github.com>
2023-11-15 14:34:29 +0000

    Add a table

--- main.py
+++ main.py
@@ -20,8 +20,31 @@ for filepath in filepaths:
     pdf.set_font(family="Times", size=16, style="B")
     # information to the page
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
-    pdf.cell(w=50, h=8, txt=f"Date {date}")
+    pdf.cell(w=50, h=8, txt=f"Date {date}", ln=1)
+
+    # Add headings to the table
+    columns = df.columns
+    columns = [item.replace("_", " ").capitalize() for item in columns]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=50, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=50, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+
     pdf.output(f"PDFs/{filename}.pdf")
+
 
 
 
